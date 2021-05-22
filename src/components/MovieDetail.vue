93f79c95c63a68863e7c383348417dcd9e0e82aa
parkeunsang <dislive@naver.com>
2021-05-22 17:24:04 +0900

    영화디테일하는중

---
+++ src/components/MovieDetail.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    
+    <div class="row">
+    <div class="col-6">
+
+    <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" class="card-img-top" alt="...">
+    </div>
+    <div class="col-6">
+        {{ genres }}
+        {{ movie.budget }}
+        {{ movie.overview }}
+        {{ movie.release_date }}
+        {{ movie.revenue }}
+        {{ movie.runtime }}
+        {{ movie.vote_average }}
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+export default {
+  name: 'MovieDetail',
+  props: {
+    movie: Object
+  },
+  computed: {
+    genres() {
+      console.log('t', this.movie.genres[0].name)
+      const genres = this.movie.genres.forEach(element => element.name)
+      console.log(genres)
+      return genres
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 5px;
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/views/MovieList.vue
+++ src/views/MovieList.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
     <NavMenu/>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">{{ movie.title }}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <MovieDetail :movie="movie"/>
+          </div>
+        </div>
+      </div>
+  </div>
+
+
     <div class="container">
 
     <div class="row">
@@ -9,7 +24,10 @@
           <div class="card-body">
             <h5 class="card-title">{{ movie.title_ko }}</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"
+            @click="getDetail(movie)">
+              Detail
+            </button>
           </div>
         </div>
       </div>
@@ -19,22 +37,35 @@
 
 <script>
 import NavMenu from '@/components/NavMenu.vue'
+import MovieDetail from '@/components/MovieDetail.vue'
 import axios from 'axios'
+const API_KEY = process.env.VUE_APP_TMDB_KEY
 export default {
   name: 'MovieList',
   components: {
     NavMenu,
+    MovieDetail
   },
   data() {
     return {
-      movies: ''
+      movies: '',
+      movie: {},
+    }
+  },
+  methods: {
+    getDetail(movie) {
+      console.log('나중에쓸거',movie)
+      const url = `https://api.themoviedb.org/3/movie/${634649}?api_key=${API_KEY}&language=ko`
+      axios.get(url)
+      .then((res) => {
+        this.movie = res.data
+      })
     }
   },
   created() {
     axios.get('http://127.0.0.1:8000/movies/')
       .then((res) => {
         this.movies = res.data
-        console.log(this.movies)
       })
     // console.log( this.$store.state.keywords.picked)
     // console.log( this.$store.state.keywords.inputValue)
