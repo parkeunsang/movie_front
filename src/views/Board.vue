23e13f5bbc4901db41af7f6b63d02a2dfe55423a
landform <landform.dev@gmail.com>
2021-05-24 15:26:49 +0900

    board

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,18 +1,37 @@
 <template>
   <div>
     <NavMenu/>
+    <div>
+      <ul>
+        <li v-for="article in articles" :key="`article_${article.id}`">
+          {{ article.title }} {{ article.id }}
+        </li>
+      </ul>
+    </div>
+    <RouterLink :to="{name: 'Create'}" tag="button"> 새글 쓰기 </RouterLink>
   </div>
 </template>
 
 <script>
 import NavMenu from '@/components/NavMenu.vue'
+import { mapGetters } from 'vuex'
+import { mapActions } from 'vuex'
 
 
 export default {
   name: 'Board',
   components: {
     NavMenu,
-    }
+    },
+  computed: {
+    ...mapGetters(['articles'])
+  },
+  methods: {
+    ...mapActions(['fetchArticles'])
+  },
+  created() {
+    this.fetchArticles()
+  }
 }
 </script>
 
